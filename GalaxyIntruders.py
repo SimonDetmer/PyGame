4a861a5db5b225444ac775bc22317fd9324b5b07
simon <simon.detmer@googlemail.com>
2024-05-20 14:34:58 +0200

    Initialize-20-05-24

---
+++ GalaxyIntruders.py
@@ -0,0 +1,36 @@
+__author__ = 'sdetmer'
+
+# Import and Initialization
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+# Display configuration
+size = (640, 480)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('GalaxyIntruders')
+# bg = pygame.image.load("images/rasen01.jpg")
+
+# Entities
+
+# Action --> ALTER
+# Assign Variables
+
+# Loop
+running = True
+while running:
+    # Timer
+
+    # Event Handling
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    # Draw the background
+  #  screen.blit(bg, (0, 0))
+
+    # Redisplay
+    pygame.display.flip()
+
+pygame.quit()
